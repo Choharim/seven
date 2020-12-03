2633a9f4b8862049556c321b9cf1136b5f6673fc
Choharim <jhl8586@naver.com>
2020-12-03 16:15:57 +0900

    Modify plus,equal

--- index.js
+++ index.js
@@ -3,62 +3,56 @@ const resultValue = document.querySelector(".result");
 const plus = document.querySelector(".plus"); 
 const minus = document.querySelector(".minus");
 const reset = document.querySelector(".reset");
+const equal = document.querySelector(".equal");
 
-let inputNumberArray = [];
-let elementNumberArray = [];
+let makeNumberArray = [];
 let totalNumberArray = [];
 
 function resetAll(){
-  elementNumberArray = [];
+  makeNumberArray = [];
   totalNumberArray = [];
   resultValue.innerText = "0";
+}
+
+function plusEqualCalc(){
+  const number = inputNumber();
+  if(number === undefined || isNaN(number) || number === 0){ //0누르고 equ눌렀거나 그냥 equ눌렀을 때 대비해서
+    return;
+  }else {
+    totalNumberArray.push(number);
+    makeNumberArray = [];
+    const accumul= totalNumberArray.reduce((result,current) => { return result + current ;});
+    showInputNumber(accumul);
+  }
 }
 
 function showInputNumber(number){
   resultValue.innerText = `${number}`;
 }
 
-function plusCalc(){
-  const elementNumber= inputNumber();
-  if(elementNumber === undefined || isNaN(elementNumber) || elementNumber === 0){
-    return;
-  }else{
-    makeElementNumberArray();
-    totalNumberArray.push(elementNumber);
-    const accumul= totalNumberArray.reduce((result,current) => { return result + current ;});
-    showInputNumber(accumul);
-    inputNumberArray = [];
-  }
-  elementNumberArray = [];
-}
-
-function makeElementNumberArray(){
-  const elementNumber = inputNumber();
-
-  elementNumberArray.push(elementNumber);
-}
-
 function inputNumber(number){
-  inputNumberArray.push(number);
-  if(inputNumberArray[0] === "0"){
-    inputNumberArray.shift();
+  makeNumberArray.push(number);
+  if(makeNumberArray[0] === "0"){
+    makeNumberArray.shift();
   }
-  const str_pressNumber = inputNumberArray.join('');
+  const str_pressNumber = makeNumberArray.join('');
   const num_pressNumber = parseInt(str_pressNumber);
 
   if(!isNaN(num_pressNumber)){
-    showInputNumber(num_pressNumber);
+    showInputNumber(num_pressNumber); //makeNumberArray에 첫 숫자 0들어가면 없애서 display에 NaN뜸.
+    return num_pressNumber
   }
-  return num_pressNumber;
 }
 
 function pressNumber(event){
   const pressNumberBtn = event.target.value;
- 
+
   inputNumber(pressNumberBtn);
 }
 
 
 numbers.forEach(number => {number.addEventListener("click",pressNumber)});
-plus.addEventListener("click",plusCalc);
+plus.addEventListener("click",plusEqualCalc);
+equal.addEventListener("click",plusEqualCalc);
 reset.addEventListener("click",resetAll);
+
